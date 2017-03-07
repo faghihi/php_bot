058c01cc5a7e36f042c7882e8c099d7e0936074c
hossein <h.faghihi15@gmail.com>
2017-03-07 22:54:22 +0330

    initialize

---
+++ set.php
@@ -0,0 +1,19 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$API_KEY = '376682828:AAE79WT571kMAmUk77iD3KJIni0ml7xixEs';
+$BOT_NAME = 'testhossein';
+$hook_url = 'https://botbot.hamyad.net/bot/hook.php';
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOgk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e;
+}
